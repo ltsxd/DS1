61f0122650ca4ef204257959d406ab8c7f326b64
ltsxd <lucas.trancoso.ds@gmail.com>
2020-05-11 13:07:49 -0300

    Aula 23/04/2020

---
+++ Exemplo/exemplo17.js
@@ -0,0 +1,195 @@
+const display = document.getElementById('display');
+
+var valorAtual = '0';
+var valor1 = 0;
+var operacao = '';
+var resetAtual = false;
+
+function limpar() {
+    valorAtual = '0';
+    valor1 = 0;
+    operacao = '';
+    resetAtual = false;
+    display.textContent = valorAtual;
+
+}
+function setOperacao(sinal) {
+    valor1 = parseFloat(valorAtual);
+    operacao = sinal;
+    resetAtual = true;
+
+}
+
+function inputValue(value) {
+    //numeros
+    if (value >= '0' && value <= 9) {
+        if (resetAtual) {
+            valorAtual = '0';
+            resetAtual = false;
+
+        }
+        if (valorAtual.length < 14) {
+            if (valorAtual == '0') {
+                valorAtual = value
+            } else {
+                valorAtual += value;
+            }
+        }
+    }
+
+    //separador decimal
+    if (value == '.') {
+        if (valorAtual.indexOf('.') == -1) {
+            valorAtual += value;
+        }
+    }
+
+    //inverter o sinal
+    if (value == '+/-') {
+        if (valorAtual.indexOf('-') == -1) {
+            valorAtual = '-' + valorAtual;
+        } else {
+            valorAtual = valorAtual.substring(1, 14);
+        }
+    }
+
+    //percentual
+    if (value == '%') {
+        var novoValor = parseFloat(valorAtual) / 100;
+        valorAtual = novoValor.toString();
+    }
+
+    display.textContent = valorAtual;
+}
+
+function calcular() {
+    var resultado = 0;
+
+    //adicao
+    if (operacao == '+') {
+        resultado = valor1 + parseFloat(valorAtual);
+    }
+
+    //subtração
+    if (operacao == '-') {
+        resultado = valor1 - parseFloat(valorAtual);
+    }
+
+    //multiplicação
+    if (operacao == 'x') {
+        resultado = valor1 . parseFloat(valorAtual);
+    }
+
+    //divisão
+    if (operacao == '/') {
+        resultado = valor1 / parseFloat(valorAtual);
+    }
+
+    //garante que o proximo digitar ira sobrepor o valor
+    //resetAtual = true;
+
+    //atualiza o display
+    resetAtual = true;
+    valorAtual = resultado.toString();
+    display.textContent = valorAtual;
+
+    //limpa a variavel operacao
+    //operacao = '';
+}const display = document.getElementById('display');
+
+var valorAtual = '0';
+var valor1 = 0;
+var operacao = '';
+var resetAtual = false;
+
+function limpar() {
+    valorAtual = '0';
+    valor1 = 0;
+    operacao = '';
+    resetAtual = false;
+    display.textContent = valorAtual;
+
+}
+function setOperacao(sinal) {
+    valor1 = parseFloat(valorAtual);
+    operacao = sinal;
+    resetAtual = true;
+
+}
+
+function inputValue(value) {
+    //numeros
+    if (value >= '0' && value <= 9) {
+        if (resetAtual) {
+            valorAtual = '0';
+            resetAtual = false;
+
+        }
+        if (valorAtual.length < 14) {
+            if (valorAtual == '0') {
+                valorAtual = value
+            } else {
+                valorAtual += value;
+            }
+        }
+    }
+
+    //separador decimal
+    if (value == '.') {
+        if (valorAtual.indexOf('.') == -1) {
+            valorAtual += value;
+        }
+    }
+
+    //inverter o sinal
+    if (value == '+/-') {
+        if (valorAtual.indexOf('-') == -1) {
+            valorAtual = '-' + valorAtual;
+        } else {
+            valorAtual = valorAtual.substring(1, 14);
+        }
+    }
+
+    //percentual
+    if (value == '%') {
+        var novoValor = parseFloat(valorAtual) / 100;
+        valorAtual = novoValor.toString();
+    }
+
+    display.textContent = valorAtual;
+}
+
+function calcular() {
+    var resultado = 0;
+
+    //adicao
+    if (operacao == '+') {
+        resultado = valor1 + parseFloat(valorAtual);
+    }
+
+    //subtração
+    if (operacao == '-') {
+        resultado = valor1 - parseFloat(valorAtual);
+    }
+
+    //multiplicação
+    if (operacao == 'x') {
+        resultado = valor1 . parseFloat(valorAtual);
+    }
+
+    //divisão
+    if (operacao == '/') {
+        resultado = valor1 / parseFloat(valorAtual);
+    }
+
+    //garante que o proximo digitar ira sobrepor o valor
+    //resetAtual = true;
+
+    //atualiza o display
+    resetAtual = true;
+    valorAtual = resultado.toString();
+    display.textContent = valorAtual;
+
+    //limpa a variavel operacao
+    //operacao = '';
+}
